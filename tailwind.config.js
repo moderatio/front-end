3fa2318553a69cdbc4827893969e8dbac15c4f15
pedro bufulin <pedro.almeida@ufu.br>
2023-06-05 12:32:05 -0300

    tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors:{
-      'ebony': '#09081B',
-      'mulberry': '#C35180',
-      'java': '#16CADC',
-      'fun-blue': '#2160B7'
+    colors: {
+      white: "#F5F5F3",
+      ebony: "#09081B",
+      mulberry: "#F25C7D",
+      java: "#05D6CA",
+      "fun-blue": "#02214B",
+      "dark-blue": "#021541",
+      "weird-orange": "#F56558",
+      "dark-pink": "#9D406E",
     },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
