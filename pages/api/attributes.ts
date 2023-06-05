fe8b91280ad6dfd1fb1d827556b35f1552541aa8
pedro bufulin <pedro.almeida@ufu.br>
2023-06-05 16:27:00 -0300

    add builder.io attributes

---
+++ pages/api/attributes.ts
@@ -0,0 +1,16 @@
+import { getAttributes } from "@builder.io/personalization-utils/dist/get-attributes";
+import { type NextApiRequest, type NextApiResponse } from "next";
+
+if (process.env.BUILDER_PRIVATE_KEY === "") {
+  throw new Error("No BUILDER_PRIVATE_KEY defined");
+}
+
+/**
+ * API to get the custom targeting attributes from Builder, only needed for the context menu to show a configurator and allow toggling of attributes
+ */
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const attributes = await getAttributes(
+    String(process.env.BUILDER_PRIVATE_KEY)
+  );
+  res.send(attributes);
+};
