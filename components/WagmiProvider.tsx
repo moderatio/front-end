bfeebcf78ac06bf7c0834b8f56586719dfef6b8d
pedro bufulin <pedro.almeida@ufu.br>
2023-06-07 18:06:04 -0300

    wagmi provider

---
+++ components/WagmiProvider.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React, { useEffect } from "react";
+import { SequenceConnector } from "@0xsequence/wagmi-connector";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider, webSocketProvider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [publicProvider()]
+);
+
+const connectors = [
+  new SequenceConnector({
+    chains,
+    options: {
+      connect: {
+        app: "Demo",
+        networkId: 137,
+      },
+    },
+  }),
+];
+
+// wagmi client setup. Note: we do not auto-connect here, but instead
+// do it when the component mounts to prevent hydration issues with wagmi +
+// next.js SSR. This is a common quirk with next+wagmi.
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+  webSocketProvider,
+});
+
+interface WagmiProviderProps {
+  children: React.ReactNode;
+}
+
+function WagmiProvider({ children }: WagmiProviderProps) {
+  // auto-connect on mount
+  useEffect(() => {
+    wagmiClient.autoConnect();
+  }, []);
+
+  return <WagmiConfig client={wagmiClient}>{children}</WagmiConfig>;
+}
+
+export default WagmiProvider;
