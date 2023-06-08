3e15ab7d2a83c263ae9cfb113413e747b30a027c
pedro bufulin <pedro.almeida@ufu.br>
2023-06-08 18:22:51 -0300

    custom modal

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,10 +1,23 @@
+import Modal from "react-modal";
+
 interface Props {
   isOpen: boolean;
   onClose: () => void;
   children: React.ReactNode;
 }
 
-const Modal = ({ isOpen, onClose, children }: Props) => {
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+const CustomModal = ({ isOpen, onClose, children }: Props) => {
   const closeModal = () => {
     onClose();
   };
@@ -13,32 +26,31 @@ const Modal = ({ isOpen, onClose, children }: Props) => {
     <>
       {isOpen && (
         <>
-          <div className="justify-center ease-in items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-            <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/* content */}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/* header and body */}
-                <div className="m-5">{children}</div>
-                {/* footer */}
-                <div className="flex items-center justify-end p-6  rounded-b">
-                  <button
-                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="button"
-                    onClick={() => {
-                      closeModal();
-                    }}
-                  >
-                    Close
-                  </button>
-                </div>
-              </div>
+          <Modal
+            style={customStyles}
+            isOpen={isOpen}
+            onRequestClose={closeModal}
+          >
+            {/* content */}
+            {/* header and body */}
+            <div className="m-5">{children}</div>
+            {/* footer */}
+            <div className="flex items-center justify-end p-6  rounded-b">
+              <button
+                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={() => {
+                  closeModal();
+                }}
+              >
+                Close
+              </button>
             </div>
-          </div>
-          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+          </Modal>
         </>
       )}
     </>
   );
 };
 
-export default Modal;
+export default CustomModal;
