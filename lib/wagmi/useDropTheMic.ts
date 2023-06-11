c0e0730a53361736cb291809ffc7ab2f6bd1b0da
pedro bufulin <pedro.almeida@ufu.br>
2023-06-11 19:01:51 -0300

    fix: lint

--- lib/wagmi/useDropTheMic.ts
+++ lib/wagmi/useDropTheMic.ts
@@ -1,6 +1,5 @@
 import { useContractWrite, usePrepareContractWrite } from "wagmi";
 import moderatioAbi from "@/abi/Moderatio.json";
-import { BigNumber } from "ethers";
 
 const contractAddress = String(process.env.NEXT_PUBLIC_MODERATIO_ADDRESS);
 
